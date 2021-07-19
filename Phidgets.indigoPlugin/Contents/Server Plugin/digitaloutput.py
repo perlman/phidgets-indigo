4d84174a4891e556bdd7d632c17163739d3bc37a
Eric Perlman <perlman@gmail.com>
2021-07-19 17:47:55 +0200

    Add digital output.

---
+++ Phidgets.indigoPlugin/Contents/Server Plugin/digitaloutput.py
@@ -0,0 +1,63 @@
+import traceback
+
+import indigo
+
+from Phidget22.PhidgetException import PhidgetException
+from Phidget22.Devices.DigitalOutput import DigitalOutput
+from Phidget22.ErrorCode import ErrorCode
+
+from phidget import PhidgetBase
+
+import phidget_util
+
+class DigitalOutputPhidget(PhidgetBase):
+    def __init__(self, *args, **kwargs):
+        super(DigitalOutputPhidget, self).__init__(phidget=DigitalOutput(), *args, **kwargs)
+
+    def updateIndigoStatus(self):
+        dutyCycle = self.phidget.getDutyCycle()
+        onOffState = bool(self.phidget.getState())
+        self.indigoDevice.updateStateOnServer("dutyCycle", value=dutyCycle)
+        self.indigoDevice.updateStateOnServer("onOffState", value=onOffState, uiValue="on" if onOffState else "off")
+
+    def addPhidgetHandlers(self):
+        self.phidget.setOnErrorHandler(self.onErrorHandler)
+        self.phidget.setOnAttachHandler(self.onAttachHandler)
+
+    def onAttachHandler(self, ph):
+        try:
+            phidget_util.logPhidgetEvent(ph, self.logger.info, "Attach")
+            ph.setDutyCycle(1.0)    # Set the duty cycle to 100% by default
+            self.updateIndigoStatus()
+        except Exception as e:
+            self.logger.error(traceback.format_exc())
+
+    def getDeviceStateList(self):
+        # Currently support the minimal states used by all phidget DigitalOutput devices
+        newStatesList = indigo.List()
+        # onOffState is automatically inherited from an Indigo relay class
+        # newStatesList.append(self.indigo_plugin.getDeviceStateDictForBoolOnOffType("onOffState", "onOffState", "onOffState"))
+        newStatesList.append(self.indigo_plugin.getDeviceStateDictForNumberType("dutyCycle", "dutyCycle", "dutyCycle"))
+        return newStatesList
+    
+    def getDeviceDisplayStateId(self):
+        return False
+        #return "onOffState"
+
+    def actionControlDevice(self, action):
+        if action.deviceAction == indigo.kDeviceAction.TurnOn:
+            self.phidget.setState_async(True, self.asyncSetResult)
+            ##self.indigoDevice.updateStateOnServer("onOffState", value=False, uiValue="on")
+        elif action.deviceAction == indigo.kDeviceAction.TurnOff:
+            self.phidget.setState_async(False, self.asyncSetResult)
+            #self.indigoDevice.updateStateOnServer("onOffState", value=False, uiValue="off")
+        elif action.deviceAction == indigo.kDeviceAction.RequestStatus:
+            self.updateIndigoStatus()
+        else:
+            self.logger.error("Unexpected action: %s" % action.deviceAction)
+
+    def asyncSetResult(self, ch, res, details):
+        if res != ErrorCode.EPHIDGET_OK:
+            self.logger.error("Async failure: %i : %s" % (res, details))
+        self.updateIndigoStatus()
+
