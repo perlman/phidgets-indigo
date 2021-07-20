b9b7c4ec3222a37f97d7eb9918e96afb1778e2bb
Eric Perlman <perlman@gmail.com>
2021-07-20 15:55:46 +0200

    Reset timer in stop to avoid message with quick cycle.

--- Phidgets22.indigoPlugin/Contents/Server Plugin/phidget.py
+++ Phidgets22.indigoPlugin/Contents/Server Plugin/phidget.py
@@ -86,6 +86,7 @@ class PhidgetBase(object):
         phidget_util.logPhidgetEvent(ph, self.logger.debug, "Attach")
 
     def stop(self):
+        self.timer.cancel()
         self.logger.debug("Stopping " + self.__class__.__name__ + " for Indigo device '" + str(self.indigoDevice.name) + "' (%d)" % self.indigoDevice.id)
         self.phidget.close()
 
